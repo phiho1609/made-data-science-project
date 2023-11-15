83b54d98bea2e65989f3603643c064f7b0e987c2
Philipp Hoffmeister <44923595+phiho1609@users.noreply.github.com>
2023-11-15 15:56:02 +0100

    Modulize pipeline

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -3,34 +3,43 @@ import pandas as pd
 from sqlalchemy import create_engine, Column, Integer, String, TEXT, REAL, INTEGER
 from sqlalchemy.ext.declarative import declarative_base
 
-print('lol')
 
-url = 'https://opendata.rhein-kreis-neuss.de/api/v2/catalog/datasets/rhein-kreis-neuss-flughafen-weltweit/exports/csv'
-csv_df = pd.read_csv(url, sep=';')
-print(csv_df.head())
-csv_df = csv_df.infer_objects()
-print(csv_df.head())
+def get_csv_from_url(url: str, sep: str):
+    # url = 'https://opendata.rhein-kreis-neuss.de/api/v2/catalog/datasets/rhein-kreis-neuss-flughafen-weltweit/exports/csv'
+    csv_df = pd.read_csv(url, sep=sep)
+    # print(csv_df.head())
+    csv_df = csv_df.infer_objects()
+    # print(csv_df.head())
+    return csv_df
 
-# sqla.create_engine('airports.sqlite', echo=True)
-engine = create_engine('sqlite:///airports.sqlite', echo=True)
-# conn = engine.connect()
+def create_sqlite_db(name: str) :
+    db_str = 'sqlite:///' + name + '.sqlite'
+    engine = create_engine(db_str, echo=True)
+    return engine
 
-# types = [csv_df[column_name].dtype.name for column_name in csv_df.columns]
-# print('types:', types)
-dtype_to_SQL_dtype_dict = {'int64': INTEGER, 'float64': REAL, 'object': TEXT}
 
-Base = declarative_base()
-class Airports(Base):
-    __tablename__ = 'airports'
+def csv_to_sqlite_db(csv_df, db_name: str, db_engine):
+    dtype_to_SQL_dtype_dict = {'int64': INTEGER, 'float64': REAL, 'object': TEXT}
+
+    Base = declarative_base()
+    class Airports(Base):
+        # __tablename__ = 'airports'
+        __tablename__ = db_name
+        
+        id = Column(Integer, primary_key=True)
+        columns = [Column(column_name, dtype_to_SQL_dtype_dict[csv_df[column_name].dtype.name]) for column_name in csv_df.columns]
+        
+        __table_args__ = {'extend_existing': False}             # If true, new data will not overwrite the table, but be appended
+        locals().update({col.name: col for col in columns})     # Allow object-variable like access to the columns (for this python script)
+        
+        
+    Base.metadata.create_all(db_engine)
+
+    csv_df.to_sql('airports', db_engine, if_exists='replace', index=False)     # do not include index as column
     
-    id = Column(Integer, primary_key=True)
-    # columns = [Column(column_name, dtype_to_SQL_dtype_dict[csv_df[column_name].dtype.name] getattr(String, csv_df[column_name].dtype.name)) for column_name in csv_df.columns]
-    columns = [Column(column_name, dtype_to_SQL_dtype_dict[csv_df[column_name].dtype.name]) for column_name in csv_df.columns]
-    
-    __table_args__ = {'extend_existing': False}             # If true, new data will not overwrite the table, but be appended
-    locals().update({col.name: col for col in columns})     # Allow object-variable like access to the columns (for this python script)
     
     
-Base.metadata.create_all(engine)
-
-csv_df.to_sql('airports', engine, if_exists='replace', index=False)     # do not include index as column
+if __name__ == '__main__':
+    csv_df = get_csv_from_url('https://opendata.rhein-kreis-neuss.de/api/v2/catalog/datasets/rhein-kreis-neuss-flughafen-weltweit/exports/csv', sep=';')
+    db_engine = create_sqlite_db('airports')
+    csv_to_sqlite_db(csv_df, 'airports', db_engine)
